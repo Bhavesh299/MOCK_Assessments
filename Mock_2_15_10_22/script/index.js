66b9679b39d2ded3d67c7e395050f88e39a78203
Bhavesh Chavhan <97518972+Bhavesh299@users.noreply.github.com>
2022-10-15 12:11:15 +0530

    Index Done
    Index Done

--- Mock_2_15_10_22/script/index.js
+++ Mock_2_15_10_22/script/index.js
@@ -19,10 +19,22 @@ function display_Data(data) {
 
         // console.log(el) ;
     var div = document.createElement("div") ;
+    div.setAttribute("class", "div1") ;
+    div.addEventListener("click", () =>{
+        Add(el)
+    });
     div.innerHTML = el ;
   
     container.append(div) ;
   })
+}
+  var pubby_Array = [];
+  const Add = (data) => {
+    var temp = data ;
+    console.log(temp) ;
+    pubby_Array.push(temp) ;
+    localStorage.setItem("pubby_array", JSON.stringify(pubby_Array)) ;
+    window.location.href= "Puppy.html" ;
 }
 
 
